46f8b2edea7fef041f9c5798458615db3a29aa3a
Tuy <tuyvu381@gmail.com>
2023-10-24 00:11:10 +0700

    sua doi

---
+++ Smartwatch/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Smartwatch.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
